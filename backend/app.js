c8f714d612fde35cc68f08313634daf6156a4964
redhatho <redha_theo@hotmail.fr>
2022-06-05 12:29:55 +0200

    tentative de masquer le cluster

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,14 @@ const app = express();
 app.use(helmet());
 
 
-mongoose.connect('mongodb+srv://redhatho:fanfic190@cluster0.euwgx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+        useUnifiedTopology: true,
+    }
+)
     .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 app.use((req, res, next) => {
